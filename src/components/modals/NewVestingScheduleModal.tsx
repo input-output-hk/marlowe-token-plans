722c093e99429f8d6a1232043fb141e02ee3ab45
Larry Adames <adames.larry@gmail.com>
2023-09-08 08:05:07 -0400

    PLT-7363 create basic new vesting form

--- src/components/modals/NewVestingScheduleModal.tsx
+++ src/components/modals/NewVestingScheduleModal.tsx
@@ -1,33 +1,22 @@
 import React from 'react';
-import Payout from '../../models/Payout';
-import Token from '../../models/Token';
+import './modal.scss';
 
 interface NewVestingScheduleModalProps {
   showModal: boolean;
   closeModal: () => void;
-  // payoutsToBePaidIds: string[];
-  // payouts: Payout[];
-  // destinationAddress: string;
-  // handleWithdrawals: () => void;
 }
 
-const extractAmount = () => {
-  // return payout.tokens.map((token: Token) => token.amount).reduce((a, b) => a + b, 0n).toString();
-};
-
 const NewVestingScheduleModal: React.FC<NewVestingScheduleModalProps> = ({ showModal, closeModal }) => {
-  // const payoutsToBePaid = payouts.filter(payout => payoutsToBePaidIds.includes(payout.payoutId));
-
   return (
     <>
-      <div className={`modal ${showModal ? 'show' : ''}`} tabIndex={-1} style={{ display: showModal ? 'block' : 'none' }}>
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
+    <div className={`modal ${showModal ? 'show' : ''}`} tabIndex={-1} style={{ display: showModal ? 'block' : 'none' }}>
+      <div className="modal-dialog modal-dialog-centered modal-lg">
+        <div className="modal-content">
             <div className="modal-header">
               <div className='container'>
                 <div className='row'>
                   <div className='col-10 text-left'>
-                    <h5 className="modal-title">CREATE VESTING SCHEDULE</h5>
+                    <h5 className="modal-title">Create vesting schedule</h5>
                   </div>
                   <div className='col-2 text-right'>
                     <button type="button" className="close btn btn-outline-secondary" onClick={closeModal}>
@@ -41,30 +30,49 @@ const NewVestingScheduleModal: React.FC<NewVestingScheduleModalProps> = ({ showM
               <div className='container'>
                 <div className='row'>
                   <div className='col-12'>
-                    <div className='container outer-modal-body'>
-                      <p className='font-weight-bold'>Reward name</p>
-                      <div className='container inner-modal-body'>
+                    <form action="">
+                      <p className='font-weight-bold mt-3'>Details</p>
+                      <div className="form-group my-3">
+                        <label htmlFor="name">Enter name/ID of your schedule</label>
+                        <input type="text" className="form-control" id="name"  />
+                      </div>
+                      <div className="form-group my-3">
+                        <label htmlFor="numberOfShares">Enter value of shares for each recipient</label>
+                        <input type="text" className="form-control" id="numberOfShares"  />
+                      </div>
+
+                      <p className='font-weight-bold mt-3'>Schedule</p>
+                      <div className="form-group my-3">
+                        <label htmlFor="startDate">Start date</label>
+                        <input type="date" className="form-control" id="startDate"  />
+                      </div>
+                      <div className="form-group my-3">
+                        <label htmlFor="endDate">End date</label>
+                        <input type="date" className="form-control" id="endDate"  />
+                      </div>
+
+                      <div className="form-group my-3">
+                        <label htmlFor="vestingCycle">Vesting cycle</label>
+                        <select className="form-control" id="vestingCycle">
+                          <option value="">Select a cycle</option>
+                          <option value="annually">Annually</option>
+                          <option value="monthly">monthly</option>
+                          {/* Add your options here */}
+                        </select>
+                      </div>
+                      <p className='font-weight-bold mt-3'>Recipients</p>
+                      <div className="form-group my-3">
+                        <label htmlFor="startDate">Add recipients to each receive the vesting schedule</label>
+                        <input type="text" placeholder="Insert or select a wallet" className="form-control" id="startDate"  />
+                      </div>
+                      <div className='wallet-list my-3'>
                         <ul>
-                          {/* {payoutsToBePaid.map((payout, index) => (
-                            <li key={index}>{payout.payoutId}: {extractAmount(payout)} lovelace</li>
-                          ))} */}
+                          <li>$BobsWallet</li>
+                          <li>$AliceWallet</li>
+                          <li>addr1v94725lv4umktv89cg2t04qjn4qq3p6l6zegvtx5es...</li>
                         </ul>
                       </div>
-                    </div>
-                  </div>
-                  <div className='col-12 my-3'>
-                    <hr className='mx-1'/>
-                    <p className='transfer-title'>
-                      Will transfer to
-                    </p>
-                    <hr className='mx-1'/>
-                  </div>
-
-                  <div className='col-12'>
-                    <p className='destination-address-title'>Your wallet address</p>
-                    <div className='container destination-address-container'>
-                      {/* <p className='destination-address'>{destinationAddress}</p> */}
-                    </div>
+                    </form>
                   </div>
                 </div>
               </div>
@@ -78,9 +86,9 @@ const NewVestingScheduleModal: React.FC<NewVestingScheduleModalProps> = ({ showM
                     </button>
                   </div>
                   <div className='col'>
-                    {/* <button type="button" className="btn btn-primary w-100" onClick={handleWithdrawals}>
-                      Confirm
-                    </button> */}
+                    <button type="button" className="btn btn-primary w-100" onClick={() => true}>
+                      Create schedule
+                    </button>
                   </div>
                 </div>
               </div>
