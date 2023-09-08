cb72f1119a9b29714c5606027c72666921df7db2
Larry Adames <adames.larry@gmail.com>
2023-09-08 08:52:20 -0400

    PLT-7363 add validation to vesting form

--- src/components/modals/NewVestingScheduleModal.tsx
+++ src/components/modals/NewVestingScheduleModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './modal.scss';
 
 interface NewVestingScheduleModalProps {
@@ -7,6 +7,79 @@ interface NewVestingScheduleModalProps {
 }
 
 const NewVestingScheduleModal: React.FC<NewVestingScheduleModalProps> = ({ showModal, closeModal }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    numberOfShares: '',
+    startDate: '',
+    endDate: '',
+    vestingCycle: '',
+    recipients: [],
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    name: null,
+    numberOfShares: null,
+    startDate: null,
+    endDate: null,
+    vestingCycle: null,
+    recipients: null,
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { id, value } = e.target;
+    setFormData({
+      ...formData,
+      [id]: value,
+    });
+  };
+
+  const handleSubmit = () => {
+    let valid = true;
+    let errors = {
+      name: null,
+      numberOfShares: null,
+      startDate: null,
+      endDate: null,
+      vestingCycle: null,
+      recipients: null,
+    };
+
+    for (const [key, value] of Object.entries(formData)) {
+      if (!value) {
+        valid = false;
+        errors = { ...errors, [key]: 'This field is required' };
+      }
+    }
+
+    setFormErrors(errors);
+
+    if (valid) {
+      // All fields are filled, proceed with form submission
+      console.log('Form submitted with', formData);
+    }
+  };
+
+  const handleClose = () => {
+    setFormData({
+      name: '',
+      numberOfShares: '',
+      startDate: '',
+      endDate: '',
+      vestingCycle: '',
+      recipients: [],
+    });
+    setFormErrors({
+      name: null,
+      numberOfShares: null,
+      startDate: null,
+      endDate: null,
+      vestingCycle: null,
+      recipients: null,
+    });
+    closeModal();
+  }
+
+
   return (
     <>
     <div className={`modal ${showModal ? 'show' : ''}`} tabIndex={-1} style={{ display: showModal ? 'block' : 'none' }}>
@@ -19,7 +92,7 @@ const NewVestingScheduleModal: React.FC<NewVestingScheduleModalProps> = ({ showM
                     <h5 className="modal-title">Create vesting schedule</h5>
                   </div>
                   <div className='col-2 text-right'>
-                    <button type="button" className="close btn btn-outline-secondary" onClick={closeModal}>
+                    <button type="button" className="close btn btn-outline-secondary" onClick={handleClose}>
                       <span aria-hidden="true">&times;</span>
                     </button>
                   </div>
@@ -30,40 +103,83 @@ const NewVestingScheduleModal: React.FC<NewVestingScheduleModalProps> = ({ showM
               <div className='container'>
                 <div className='row'>
                   <div className='col-12'>
-                    <form action="">
+                    <form>
                       <p className='font-weight-bold mt-3'>Details</p>
                       <div className="form-group my-3">
                         <label htmlFor="name">Enter name/ID of your schedule</label>
-                        <input type="text" className="form-control" id="name"  />
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="name"
+                          value={formData.name}
+                          onChange={handleInputChange}
+                        />
+                        {formErrors.name && <small className="text-danger">{formErrors.name}</small>}
                       </div>
                       <div className="form-group my-3">
                         <label htmlFor="numberOfShares">Enter value of shares for each recipient</label>
-                        <input type="text" className="form-control" id="numberOfShares"  />
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="numberOfShares"
+                          value={formData.numberOfShares}
+                          onChange={handleInputChange}
+                        />
+                        {formErrors.numberOfShares && <small className="text-danger">{formErrors.numberOfShares}</small>}
                       </div>
 
                       <p className='font-weight-bold mt-3'>Schedule</p>
                       <div className="form-group my-3">
                         <label htmlFor="startDate">Start date</label>
-                        <input type="date" className="form-control" id="startDate"  />
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="startDate"
+                          value={formData.startDate}
+                          onChange={handleInputChange}
+                        />
+                        {formErrors.startDate && <small className="text-danger">{formErrors.startDate}</small>}
                       </div>
                       <div className="form-group my-3">
                         <label htmlFor="endDate">End date</label>
-                        <input type="date" className="form-control" id="endDate"  />
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="endDate"
+                          value={formData.endDate}
+                          onChange={handleInputChange}
+                        />
+                        {formErrors.endDate && <small className="text-danger">{formErrors.endDate}</small>}
                       </div>
 
                       <div className="form-group my-3">
                         <label htmlFor="vestingCycle">Vesting cycle</label>
-                        <select className="form-control" id="vestingCycle">
+                        <select
+                          value={formData.vestingCycle}
+                          className="form-control"
+                          id="vestingCycle"
+                          onChange={handleInputChange}
+                        >
                           <option value="">Select a cycle</option>
                           <option value="annually">Annually</option>
                           <option value="monthly">monthly</option>
                           {/* Add your options here */}
                         </select>
+                        {formErrors.vestingCycle && <small className="text-danger">{formErrors.vestingCycle}</small>}
                       </div>
                       <p className='font-weight-bold mt-3'>Recipients</p>
                       <div className="form-group my-3">
-                        <label htmlFor="startDate">Add recipients to each receive the vesting schedule</label>
-                        <input type="text" placeholder="Insert or select a wallet" className="form-control" id="startDate"  />
+                        <label htmlFor="recipients">Add recipients to each receive the vesting schedule</label>
+                        <input
+                          type="text"
+                          placeholder="Insert or select a wallet" 
+                          className="form-control"
+                          id="recipients"
+                          value={formData.recipients}
+                          onChange={handleInputChange}
+                        />
+                        {formErrors.recipients && <small className="text-danger">{formErrors.recipients}</small>}
+  
                       </div>
                       <div className='wallet-list my-3'>
                         <ul>
@@ -81,12 +197,12 @@ const NewVestingScheduleModal: React.FC<NewVestingScheduleModalProps> = ({ showM
               <div className='container'>
                 <div className='row'>
                   <div className='col'>
-                    <button type="button" className="btn btn-outline-secondary w-100" onClick={closeModal}>
+                    <button type="button" className="btn btn-outline-secondary w-100" onClick={handleClose}>
                       Cancel
                     </button>
                   </div>
                   <div className='col'>
-                    <button type="button" className="btn btn-primary w-100" onClick={() => true}>
+                    <button type="button" className="btn btn-primary w-100" onClick={handleSubmit}>
                       Create schedule
                     </button>
                   </div>
