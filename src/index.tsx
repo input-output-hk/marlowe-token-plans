ec2636ab1288e758e00e63dbf9a5deae80834d16
Larry Adames <adames.larry@gmail.com>
2023-10-18 02:17:46 -0400

    PLT-7616 set api call for config json needed for dynamic setting of m… (#10)
    Co-authored-by: Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -7,16 +7,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './styles/main.scss';
 
+import { mkRestClient } from "@marlowe.io/runtime-rest-client";
 
-//  2) Get a reference to the div with ID root
-const el = document.getElementById('root');
+let runtimeURL = process.env.MARLOWE_RUNTIME_WEB_URL;
+await fetch('/config.json').then(async (res) => {
+  if (res.status === 200) {
+    const { marloweWebServerUrl } = await res.json();
+    if (!!marloweWebServerUrl) {
+      runtimeURL = marloweWebServerUrl;
+    }
+  }
+});
 
-if (!el) {
-  throw new Error('Root element not found');
+if (runtimeURL === undefined || runtimeURL === null) {
+  alert("Missing valid config.json file with marloweWebServerUrl OR env keys are not set!")
+} else {
+  const restClient = mkRestClient(runtimeURL)
+  const hasValidRuntimeInstance = await restClient.healthcheck()()
+
+  if (!hasValidRuntimeInstance) {
+    alert("Invalid runtime instance set!")
+  } else {
+    //  2) Get a reference to the div with ID root
+    const el = document.getElementById('root');
+
+    if (!el) {
+      throw new Error('Root element not found');
+    }
+
+    //  3) Tell React to take control of that element
+    const root = ReactDOM.createRoot(el);
+
+    // 4) Show the component on the screen
+    root.render(<App runtimeURL={runtimeURL} />);
+  }
 }
-
-//  3) Tell React to take control of that element
-const root = ReactDOM.createRoot(el);
-
-// 4) Show the component on the screen
-root.render(<App />);
